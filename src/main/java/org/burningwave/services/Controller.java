59533ccf18d5fe0b9a9321624041599becc4e414
Roberto Gentili <roberto.gentili.1980@gmail.com>
2022-01-21 16:42:25 +0100

    Update

--- src/main/java/org/burningwave/services/Controller.java
+++ src/main/java/org/burningwave/services/Controller.java
@@ -3,7 +3,6 @@ package org.burningwave.services;
 import java.io.IOException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.Random;
 import java.util.concurrent.ExecutionException;
 
 import javax.servlet.http.HttpServletResponse;
@@ -77,7 +76,7 @@ public class Controller {
 		response.setHeader("Cache-Control", "no-store");
 		String label = "artifact downloads";
 		return badge.build(
-			getTotalDownloadsOrNull(artifactId, startDate, months) + new Random().nextInt(10000),
+			getTotalDownloadsOrNull(artifactId, startDate, months),
 			artifactId != null ? artifactId + " " + label : label,
 			label,
 			"#4c1",
