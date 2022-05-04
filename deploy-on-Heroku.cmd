75080bd580b2f4a9069ef8a34c10fe5afa9815ce
Roberto Gentili <roberto.gentili.1980@gmail.com>
2022-05-04 16:00:57 +0200

    Added batch for deploy

---
+++ deploy-on-Heroku.cmd
@@ -0,0 +1,17 @@
+@echo off
+
+heroku login
+
+SET currentDir=%~dp0
+cd %currentDir%
+SET currentUnit=%currentDir:~0,2%
+%currentUnit%
+
+
+::To retrieve current directory name
+::for %%I in (.) do set CurrDirName=%%~nxI
+::echo %CurrDirName%
+
+heroku git:remote -a shared-software-off
+git commit --allow-empty -m "Deploy on Heroku"
+git push heroku main
